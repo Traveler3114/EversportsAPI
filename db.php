f634ff53d7587008fcafcc98603ec8adae751445
Traveler3114 <fasterthanblaster@gmail.com>
2025-03-26 17:49:26 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,10 @@
 <?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'eversportsdb');
 function openConnection() {
-    $conn = new mysqli("localhost", "apiuser", "PInterfaceHProgrammingPApplication40#", "eversportsdb");
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
